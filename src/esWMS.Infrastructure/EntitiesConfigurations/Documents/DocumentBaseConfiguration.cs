9dc40d6e7c2e475ba9ca8dc13c98b77250033fc5
Michał Mrowiec <michalmro210101@gmail.com>
2024-07-17 18:07:45 +0200

    Correct locate Infrastructure project

---
+++ src/esWMS.Infrastructure/EntitiesConfigurations/Documents/DocumentBaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using esMWS.Domain.Entities.Documents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace esWMS.Infrastructure.EntitiesConfigurations.Documents
+{
+    internal class DocumentBaseConfiguration : IEntityTypeConfiguration<DocumentBase>
+    {
+        public void Configure(EntityTypeBuilder<DocumentBase> builder)
+        {
+            
+        }
+    }
+}
---
+++ src/esWMS.Infrastructure/InfrastructureInstallation.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace esWMS.Infrastructure
+{
+    public static class InfrastructureInstallation
+    {
+        public static void AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+            services.AddDbContext<EsWmsDbContext>(
+                dbContextOptions => dbContextOptions
+                    .UseMySql(configuration.GetConnectionString("ContainerDb"), serverVersion));
+        }
+    }
+}
