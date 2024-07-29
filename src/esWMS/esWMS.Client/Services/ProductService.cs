3f883a8137ed263f95a4fba81c290ff558c1498a
Michał Mrowiec <michalmro210101@gmail.com>
2024-07-29 19:51:09 +0200

    Add get products in UI via get-filtered POST method

--- src/esWMS/esWMS.Client/Services/ProductService.cs
+++ src/esWMS/esWMS.Client/Services/ProductService.cs
@@ -1,11 +1,14 @@
 ﻿using esWMS.Client.ViewModels;
+using Newtonsoft.Json;
 using System.Net.Http.Json;
+using System.Text;
 
 namespace esWMS.Client.Services
 {
     public interface IProductService
     {
         Task<IEnumerable<ProductVM>> GetProduct();
+        Task<PagedResultVM<ProductVM>> GetProduct2(SieveModelVM sieveModel);
     }
 
     public class ProductService : IProductService
@@ -22,6 +25,24 @@ namespace esWMS.Client.Services
             var response = await _httpClient.GetFromJsonAsync<BaseServerResponseVM<PagedResultVM<ProductVM>>>("api/v1/product");
 
             return response?.ReturnedObj?.Items ?? [];
+        }
+
+        public async Task<PagedResultVM<ProductVM>> GetProduct2(SieveModelVM sieveModel)
+        {
+            //var response = await _httpClient.GetFromJsonAsync<BaseServerResponseVM<PagedResultVM<ProductVM>>>("api/v1/product");
+
+            //var response = await _httpClient.GetAsync("api/v1/product");
+            var postJson = new StringContent(JsonConvert.SerializeObject(sieveModel), Encoding.UTF8, "application/json");
+
+            using var request = new HttpRequestMessage(HttpMethod.Post, "api/v1/product/get-filtered");
+            request.Content = postJson;
+
+            var response = await _httpClient.SendAsync(request);
+
+            var json = await response.Content.ReadAsStringAsync();
+            var responseObj = JsonConvert.DeserializeObject<BaseServerResponseVM<PagedResultVM<ProductVM>>>(json);
+
+            return responseObj?.ReturnedObj ?? new();
         }
     }
 }
