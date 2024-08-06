b27c3e86073bb4293aff4a55a900b8e9a58df15e
Michał Mrowiec <michalmro210101@gmail.com>
2024-08-06 21:01:22 +0200

    Refactor table filter

---
+++ src/esWMS/esWMS.Client/Components/TableColumnsFilters/FilterOption.cs
@@ -0,0 +1,23 @@
+﻿namespace esWMS.Client.Components.TableColumnsFilters
+{
+    public class FilterOption
+    {
+        public FilterOption(string column, string @operator = "@=")
+        {
+            Column = column;
+            Operator = @operator;
+            Value = "";
+        }
+
+        public FilterOption(string column, string @operator, string value)
+        {
+            Column = column;
+            Operator = @operator;
+            Value = value;
+        }
+
+        public string Column { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+    }
+}
