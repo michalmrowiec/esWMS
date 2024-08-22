ea0042729b524a133202b3dd4bacdce24edd0b71
Michał Mrowiec <michalmro210101@gmail.com>
2024-08-22 12:11:39 +0200

    Add MmmTable to UI

---
+++ src/esWMS/esWMS.Client/ViewModels/Documents/MmmVM.cs
@@ -0,0 +1,10 @@
+﻿namespace esWMS.Client.ViewModels.Documents
+{
+    public class MmmVM : BaseDocumentVM
+    {
+        public DateTime? GoodsReleaseDate { get; set; }
+        public string ToWarehouseId { get; set; } = null!;
+
+        public WarehouseVM? ToWarehouse { get; set; }
+    }
+}
