b08f226ce34aa67d0b3357794cfdcce78ea5c79e
Michał Mrowiec <michalmro210101@gmail.com>
2024-10-20 19:07:31 +0200

    Add update zone to UI

---
+++ src/esWMS/esWMS.Client/ViewModels/WarehouseEnviroment/Zone/UpdateZoneVM.cs
@@ -0,0 +1,9 @@
+﻿namespace esWMS.Client.ViewModels
+{
+    public class UpdateZoneVM
+    {
+        public string ZoneId { get; set; } = null!;
+        public string? ZoneName { get; set; }
+        public decimal? AvgTemperature { get; set; }
+    }
+}
---
+++ src/esWMS/esWMS.Client/ViewModels/WarehouseEnviroment/Zone/ZoneVM.cs
@@ -0,0 +1,16 @@
+﻿using esWMS.Client.ViewModels.WarehouseEnviroment.Location;
+
+namespace esWMS.Client.ViewModels
+{
+    public class ZoneVM
+    {
+        public string ZoneId { get; set; } = null!;
+        public string? ZoneName { get; set; }
+        public char ZoneAlias { get; set; }
+        public string WarehouseId { get; set; } = null!;
+        public decimal? AvgTemperature { get; set; }
+
+        public WarehouseVM? Warehouse { get; set; }
+        public IList<LocationVM> Locations { get; set; } = [];
+    }
+}
