c1e39a6d1281fe2e61ebd6d0b65898eeb14c214c
Michał Mrowiec <michalmro210101@gmail.com>
2024-10-21 16:12:19 +0200

    Add DeleteZoneCommand

---
+++ src/esWMS.Application/Functions/Zones/Commands/DeleteZone/DeleteZoneValidator.cs
@@ -0,0 +1,23 @@
+﻿using esWMS.Application.Functions.Locations;
+using FluentValidation;
+
+namespace esWMS.Application.Functions.Zones.Commands.DeleteZone
+{
+    internal class DeleteZoneValidator : AbstractValidator<DeleteZoneCommand>
+    {
+        public DeleteZoneValidator(IEnumerable<LocationDto> locations)
+        {
+            RuleFor(x => x.ZoneId)
+                .NotEmpty()
+                .Custom((value, context) =>
+                {
+                    if (locations.Any())
+                    {
+                        context.AddFailure(
+                            "ZoneId",
+                            $"Cannot delete zone because it has locations: {string.Join(',', locations.Select(x => x.LocationId))}");
+                    }
+                });
+        }
+    }
+}
