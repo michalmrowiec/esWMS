c975b43010809fdfabd28ad96c2c3186f87c8652
Michał Mrowiec <michalmro210101@gmail.com>
2024-10-22 07:46:19 +0200

    Add IsActive support to UI storage

--- src/esWMS/esWMS.Client/ViewModels/WarehouseEnviroment/WarehouseVM.cs
+++ src/esWMS/esWMS.Client/ViewModels/WarehouseEnviroment/WarehouseVM.cs
@@ -9,6 +9,8 @@
         public string? Region { get; set; }
         public string? PostalCode { get; set; }
         public string? Address { get; set; }
+        public bool IsActive { get; set; }
+
 
         public IList<ZoneVM> Zones { get; set; } = [];
     }
