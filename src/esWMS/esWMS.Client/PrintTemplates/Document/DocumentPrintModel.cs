9c30dbd46cd09a503a51cbaa8cb2f0d95a63e043
Michał Mrowiec <michalmro210101@gmail.com>
2024-10-29 19:48:14 +0100

    Improve DocumentPrintModel

--- src/esWMS/esWMS.Client/PrintTemplates/Document/DocumentPrintModel.cs
+++ src/esWMS/esWMS.Client/PrintTemplates/Document/DocumentPrintModel.cs
@@ -1,4 +1,6 @@
 ﻿using esWMS.Client.ViewModels.Documents;
+using esWMS.Client.ViewModels.SystemActors;
+using esWMS.Client.ViewModels.WarehouseEnviroment.Warehouse;
 
 namespace esWMS.Client.PrintTemplates.Document
 {
@@ -8,26 +10,53 @@ namespace esWMS.Client.PrintTemplates.Document
             BaseDocumentVM model,
             DocumentTypesName documentType,
             DocumentContractorType contractorType,
-            string issueWarehouseName,
-            string issueWarehouseAddress,
-            string issueWarehousePostalCodeCity,
-            string issueWarehouseVatId,
-            string contractorName,
-            string contractorAddress,
-            string contractorPostalCodeCity,
-            string contractorVatId)
+            ContractorVM contractor)
         {
             Model = model;
             DocumentType = documentType;
             ContractorType = contractorType;
-            IssueWarehouseName = issueWarehouseName;
-            IssueWarehouseAddress = issueWarehouseAddress;
-            IssueWarehousePostalCodeCity = issueWarehousePostalCodeCity;
-            IssueWarehouseVatId = issueWarehouseVatId;
-            ContractorName = contractorName;
-            ContractorAddress = contractorAddress;
-            ContractorPostalCodeCity = contractorPostalCodeCity;
-            ContractorVatId = contractorVatId;
+            IssueWarehouseName = model.IssueWarehouse?.WarehouseName ?? "";
+            IssueWarehouseAddress = model.IssueWarehouse?.Address ?? "";
+            IssueWarehousePostalCodeCity = $"{model.IssueWarehouse?.PostalCode} {model.IssueWarehouse?.City}";
+            IssueWarehouseVatId = "";
+            ContractorName = contractor.ContractorName;
+            ContractorAddress = contractor.Address ?? "";
+            ContractorPostalCodeCity = $"{contractor.PostalCode} {contractor.City}";
+            ContractorVatId = contractor?.VatId ?? "";
+        }
+
+        public DocumentPrintModel(
+            BaseDocumentVM model,
+            WarehouseVM targetWarehouse,
+            DocumentTypesName documentType)
+        {
+            Model = model;
+            DocumentType = documentType;
+            ContractorType = DocumentContractorType.Receiver;
+            IssueWarehouseName = model.IssueWarehouse?.WarehouseName ?? "";
+            IssueWarehouseAddress = model.IssueWarehouse?.Address ?? "";
+            IssueWarehousePostalCodeCity = $"{model.IssueWarehouse?.PostalCode} {model.IssueWarehouse?.City}";
+            IssueWarehouseVatId = "";
+            ContractorName = targetWarehouse.WarehouseName;
+            ContractorAddress = targetWarehouse.Address ?? "";
+            ContractorPostalCodeCity = $"{targetWarehouse.PostalCode} {targetWarehouse.City}";
+            ContractorVatId = "";
+        }
+
+        public DocumentPrintModel(
+            BaseDocumentVM model,
+            DocumentTypesName documentType,
+            DocumentContractorType contractorType,
+            string targetOrSourceName)
+        {
+            Model = model;
+            DocumentType = documentType;
+            ContractorType = contractorType;
+            IssueWarehouseName = model.IssueWarehouse?.WarehouseName ?? "";
+            IssueWarehouseAddress = model.IssueWarehouse?.Address ?? "";
+            IssueWarehousePostalCodeCity = $"{model.IssueWarehouse?.PostalCode} {model.IssueWarehouse?.City}";
+            IssueWarehouseVatId = "";
+            ContractorName = targetOrSourceName;
         }
 
         public BaseDocumentVM Model { get; set; }
@@ -38,9 +67,9 @@ namespace esWMS.Client.PrintTemplates.Document
         public string IssueWarehousePostalCodeCity { get; set; }
         public string IssueWarehouseVatId { get; set; }
         public string ContractorName { get; set; }
-        public string ContractorAddress { get; set; }
-        public string ContractorPostalCodeCity { get; set; }
-        public string ContractorVatId { get; set; }
+        public string? ContractorAddress { get; set; }
+        public string? ContractorPostalCodeCity { get; set; }
+        public string? ContractorVatId { get; set; }
     }
 
     public enum DocumentTypesName
@@ -57,6 +86,8 @@ namespace esWMS.Client.PrintTemplates.Document
     public enum DocumentContractorType
     {
         Receiver,
-        Supplier
+        Supplier,
+        Source,
+        Target
     }
 }
