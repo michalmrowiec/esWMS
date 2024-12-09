29eefd811e3d4a1c8e83ffb793e716b9e7a18257
Michał Mrowiec <michalmro210101@gmail.com>
2024-12-09 23:02:02 +0100

    Improve quantity format in UI

--- src/Client/esWMS.Client/Services/DisplayService/NumericFormat.cs
+++ src/Client/esWMS.Client/Services/DisplayService/NumericFormat.cs
@@ -1,20 +1,24 @@
-﻿namespace esWMS.Client.Services.DisplayService
+﻿using System.Security;
+
+namespace esWMS.Client.Services.DisplayService
 {
     public static class NumericFormat
     {
+        public const string QuantityStringFormat = "F2";
+        public const string PriceStringFormat = "F2";
         public static string FormatQuantity(this decimal quantity)
         {
-            return quantity.ToString("F2");
+            return quantity.ToString(QuantityStringFormat);
         }
 
         public static string FormatPrice(this decimal quantity)
         {
-            return quantity.ToString("F2");
+            return quantity.ToString(PriceStringFormat);
         }
 
         public static string FormatPrice(this decimal? quantity)
         {
-            return quantity?.ToString("F2") ?? "";
+            return quantity?.ToString(PriceStringFormat) ?? string.Empty;
         }
     }
 }
